6e1c682a1df18ef5ea6d91c249b928940cf9bbef
naitro2010 <108594252+naitro2010@users.noreply.github.com>
2024-10-18 00:23:54 -0700

    Update build.rs

--- SkyCloth/build.rs
+++ SkyCloth/build.rs
@@ -6,10 +6,10 @@ fn main() {
     let crate_dir = env::var("CARGO_MANIFEST_DIR").unwrap();
     
     cbindgen::Builder::new()
-        .with_crate(crate_dir)
+        .with_crate(crate_dir.clone())
         .generate()
         .expect("Unable to generate bindings")
         .write_to_file("../src/mudbindings.h");
-    let mut ocl_path=std::path::PathBuf::from(crate_dir);
+    let mut ocl_path=std::path::PathBuf::from(crate_dir.clone());
     println!("cargo:rustc-link-search=native={}", ocl_path.display());
 }
